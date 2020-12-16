f4ac76470708de301f8d15c889759a1c7d3d2954
David GARDIE <74535282+Animus-Necandi@users.noreply.github.com>
2020-12-16 14:20:28 +0100

    Delete logout.js

--- scripts/logout.js
+++
@@ -1,9 +0,0 @@
-const btnLogout = document.getElementById('btn-deconnexion');
-
-let email = JSON.parse(localStorage.getItem('user'))
-
-// Cette fonction nettoie le localStorage
-btnLogout.addEventListener('click', function (){
-    localStorage.clear('user');
-    location.href = 'index.html';
-})
